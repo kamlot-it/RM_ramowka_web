c45f590cb53c676e125733edf3bead0ff65de79d
Kamlot <kamil@podejma.pl>
2025-09-23 21:00:47 +0200

    Added Hosts and Guests for programs Fixed compatibility to handle the above changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1346102969"
+    serviceWorkerVersion: "1531475277"
   }
 });
