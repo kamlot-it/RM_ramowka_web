e6325ddf10cb479ee7f2387ba316468d049c103b
Kamlot <kamil@podejma.pl>
2025-11-02 22:10:25 +0100

    Fix icon display and version issue on web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "161592226"
+    serviceWorkerVersion: "4230244998"
   }
 });
